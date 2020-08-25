22784f370369b0bb11000bd2f35d9f71d8d6cbb1
Frances O'Leary <fraoleary@gmail.com>
2020-08-24 20:11:58 -0400

    Pi approximation

---
+++ learning-exercises/piApproxCirc.m
@@ -0,0 +1,21 @@
+%{
+    Frances O'Leary, 8/24/2020
+
+    A script used approximate pi using
+    random points dropped in a square (a simple
+    Monte Carlo method). From Numerical Methods, Greenbaum.
+%}
+
+N = input('Enter the number of points: ');
+
+numberin = 0;                       % our variable to count the number of points that fall inside the circle
+for i=1:N                           % iterate through the inputted number of points and 'drop' that many
+    x = 2 * rand - 1;               % pick a random x from -1 to 1
+    y = 2 * rand - 1;               % pick a random y from -1 to 1
+    if x^2 + y^2 < 1                % determine if (x, y) is in the circle, if it is add on
+        numberin = numberin + 1;
+    end
+end
+
+pio4 = numberin / N;                % take our ratio which should approximate pi/4
+piapproc = 4 * pio4                 % multiply by 4 to give an approximation for pi; print to console.
